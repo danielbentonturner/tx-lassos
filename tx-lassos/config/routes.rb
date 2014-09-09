8b17d4d72dfaf7b59930b8e7db55358d572b550b
Joey G <digitaljedi@outlook.com>
2014-09-08 20:11:37 -0500

    fixed a couple things right quick

--- tx-lassos/config/routes.rb
+++ tx-lassos/config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   get "log_out" => "sessions#destroy", :as => "log_out"
   get "log_in" => "sessions#new", :as => "log_in"
-  get "sign_up" => "users#new", :as => "sign_up"
-  root :to => "users#new"
+  get "sign_up" => "user#new", :as => "sign_up"
+  root :to => "user#new"
   resources :users
   resources :sessions
   # The priority is based upon order of creation: first created -> highest priority.
