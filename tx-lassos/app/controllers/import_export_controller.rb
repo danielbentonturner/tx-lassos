67308e7997e87a3abdce283858c8fca02d127d47
daniel <demise4u@gmail.com>
2014-09-09 20:23:48 -0500

    added xls template

--- tx-lassos/app/controllers/import_export_controller.rb
+++ tx-lassos/app/controllers/import_export_controller.rb
@@ -6,7 +6,13 @@ class ImportExportController < ApplicationController
     respond_to do |format|
       format.html
       format.csv { send_data @users.export}
+      format.xls {send_data @users.export(col_sep: "\t")}
     end
+  end
+
+  def import
+    User.import(params[:file])
+    redirect_to SOMETHING, flash: "User data imported."
   end
 
 end
--- tx-lassos/app/models/user.rb
+++ tx-lassos/app/models/user.rb
@@ -3,14 +3,16 @@ class User < ActiveRecord::Base
   include Gravtastic
   gravtastic
 
-  def self.import(file)
+  def self.import
     CSV.foreach(file.path, headers: true) do |row|
-      Users.create! row.to_hash
+      user = find_by_id(row["id"]) || new
+      user.attributes = row.to_hash.slice(*accessible_attributes)
+      user.save!
     end
   end
 
-  def self.export
-    CSV.generate do |csv|
+  def self.export(options = {})
+    CSV.generate(options) do |csv|
       csv << column_names
       all.each do |item|
         csv << item.attributes.values_at(*column_names)
