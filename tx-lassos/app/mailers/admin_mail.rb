2c448c691ee3c6e3a5f508b46ce8fa99273c9246
Justin Beuck <jbeuck@yahoo.com>
2014-09-10 16:50:37 -0500

    added confirmation email logic

---
+++ tx-lassos/app/mailers/admin_mail.rb
@@ -0,0 +1,20 @@
+class AdminMailer < ActionMailer::Base
+	default from: "txlassos@gmail.com"
+	email_array = []
+
+	def find_admin(user)
+		@users = User.all
+		@users.each do |user|
+			if user.admin
+				email_array << user.email
+			end
+		end
+	end
+
+	def new_registrant
+		email_array.each do |i|
+			mail(to: i, subject: 'test admin email')
+		end
+	end
+end
+end
