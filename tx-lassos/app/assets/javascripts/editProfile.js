24df066be3fce4e92e2023e8d891a71f2d1ebdbe
Joey G <digitaljedi@outlook.com>
2014-09-11 20:32:17 -0500

    fixed controller to write from profile to db

--- tx-lassos/app/assets/javascripts/editProfile.js
+++ tx-lassos/app/assets/javascripts/editProfile.js
@@ -1,5 +1,22 @@
-$("#comm").on('click', function(e) {
-	e.preventDefault;
-	$(".comments-area").hide();
-	$("#text-area").show();
-})
+$(document).on('ready', function() {
+	$(".comments-area").show();
+	$("#text-area").hide();
+	$(".social-form").hide();
+
+	$("#comm").on('click', function(e) {
+		e.preventDefault;
+		$(".comments-area").hide();
+		$("#comm").hide();
+		$("#text-area").show();
+		$('#comm-save').addClass("btn btn-primary")
+	});
+
+	$("#con").on('click', function() {
+		$(".social-form").show();
+		$(".visible-social").hide();
+		$("#con").hide();
+	});
+
+	
+});
+
