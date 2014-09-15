2391bfe908f66f68880750d692769fa472f012e1
daniel <demise4u@gmail.com>
2014-09-15 10:24:56 -0500

    added adminedit power

--- tx-lassos/app/models/user.rb
+++ tx-lassos/app/models/user.rb
@@ -23,14 +23,14 @@ class User < ActiveRecord::Base
     User.where(approved: false)
   end
 
-  def self.export(options = {})
-    CSV.generate do |csv|
-      csv << column_names
-      all.each do |user|
-        csv << user.attributes.values_at(*column_names)
-      end
-    end
-  end
+  # def self.export(options = {})
+  #   CSV.generate do |csv|
+  #     csv << column_names
+  #     all.each do |user|
+  #       csv << user.attributes.values_at(*column_names)
+  #     end
+  #   end
+  # end
 
   include Gravtastic
   gravtastic
